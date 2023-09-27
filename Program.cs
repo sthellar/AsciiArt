1a35623065729c31686a67da1930758c8943316f
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-09-27 18:39:57 -0300

    Adicionar solução da atividade AsciiArt.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(" ||  || ");
+Console.WriteLine(" \\\\()//");
+Console.WriteLine("//(__)\\\\");
+Console.WriteLine("||    ||");
+Console.ResetColor();
